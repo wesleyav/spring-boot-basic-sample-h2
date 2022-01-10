f2292a3c9985058e4f9ea5c40bfb996183dab58a
Wesley Araujo Vieira <wesley.contato@hotmail.com>
2022-01-10 19:36:23 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/github/wesleyav/springbootbasicsampleh2/SpringBootBasicSampleH2Application.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.springbootbasicsampleh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBasicSampleH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBasicSampleH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/wesleyav/springbootbasicsampleh2/SpringBootBasicSampleH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.springbootbasicsampleh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicSampleH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
